32deed0b58f9fa1d6179429f2d1e0151a2ce6a3c
elizabeth <elizabethmarymathew24@gmail.com>
2025-12-25 19:20:23 +0530

    chore: adds LOADING STATES as common type

---
+++ stream-frontend/src/common.jsx
@@ -0,0 +1,16 @@
+
+export const TIME_RANGES = [
+    { label: 'Last 5 minutes', seconds: 300 },
+    { label: 'Last 10 minutes', seconds: 600 },
+    { label: 'Last 30 minutes', seconds: 1800 },
+    { label: 'Last 60 minutes', seconds: 3600 },
+    { label: 'Last 2 hours', seconds: 7200 },
+    { label: 'All time', seconds: 86400 }, //TODO
+  ]
+
+  export const LOADING_STATUS = {
+    IDLE: 'idle',
+    LOADING: 'loading',
+    SUCCESS: 'success',
+
+  }
